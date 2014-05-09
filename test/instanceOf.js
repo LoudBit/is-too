2be2bfadc7dcc985c08e2c0a94b1215acfe99d0c
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-05-09 15:48:56 -0500

    add instanceOf method

---
+++ test/instanceOf.js
@@ -0,0 +1,47 @@
+var is = require('../is-too')
+var assert = require('assert')
+var asserts = require('./_asserts')
+
+var instanceOfObject = [
+  'args',
+  'emptyArray',
+  'emptyObject',
+  'fn',
+  'newArray',
+  'newBooleanFalse',
+  'newBooleanTrue',
+  'newDate',
+  'newObject',
+  'newRegExp',
+  'objectCreateObjectPrototype',
+  'objectLengthOne',
+  'plainArray',
+  'plainObject',
+  'regex'
+]
+
+var instanceOfArray = [
+  'emptyArray',
+  'newArray',
+  'plainArray'
+]
+
+describe('is.instanceOf', function(){
+
+  it('undefined standard', function(){
+    asserts.standard('instanceOf', [], undefined)
+  })
+
+  it('Object standard', function(){
+    asserts.standard('instanceOf', instanceOfObject, Object)
+  })
+
+  it('Array standard', function(){
+    asserts.standard('instanceOf', instanceOfArray, Array)
+  })
+
+  it('not.instanceOf', function(){
+    asserts.notStandard('instanceOf', instanceOfArray, Array)
+  })
+
+})
