d603478ba282d2beb619b09ea29d4a8ae6cd7504
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-12-09 14:20:13 -0600

    Add test for is.match method.

--- test/regex.js
+++ test/regex.js
@@ -1,6 +1,9 @@
 var asserts = require('./_asserts')
+var assert = require('assert')
+var is = require('../is-too')
 
 var regexs = ['regex', 'newRegExp']
+var rx = /foo/
 
 describe('regular expressions', function(){
 
@@ -10,6 +13,15 @@ describe('regular expressions', function(){
 
   it('is.not.regex', function(){
     asserts.not('regex', regexs)
+  })
+
+  it('is.match', function(){
+    assert.equal(true, is.match('foo', rx))
+  })
+
+  it('is.not.match', function(){
+    assert.equal(true, is.not.match('bar', rx))
+    assert.equal(true, is.not.match(new Date(), rx))
   })
 
 })
